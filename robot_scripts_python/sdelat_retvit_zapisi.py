7c041353f5a4c3b1030aac1b21a9cd119e37ab13
zhernosek12 <zhernosek12@gmail.com>
2023-01-04 01:08:37 +0300

    Update sdelat_retvit_zapisi.py

--- robot_scripts_python/sdelat_retvit_zapisi.py
+++ robot_scripts_python/sdelat_retvit_zapisi.py
@@ -1,7 +1,7 @@
-    driver.get(url)
+driver.get(url)
 
-    time.sleep(5)
+time.sleep(5)
 
-    find_element_by_xpath_v2(driver, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span/span').click()
+find_element_by_xpath_v2(driver, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span/span').click()
 
-    time.sleep(3)
+time.sleep(3)
