a07adc228d3fd73dc285f0cb6730a1d20700ef8e
zhernosek12 <zhernosek12@gmail.com>
2023-01-11 20:59:25 +0300

    Create rgeistraciya_v_forme_toshies.py

---
+++ robot_scripts_python/rgeistraciya_v_forme_toshies.py
@@ -0,0 +1,20 @@
+# скрипт запускается с http://ferma.zhernosek.xyz и имеет все встроенные модули
+# https://t.me/zhernosek12
+
+driver.get("https://toshies.xyz/whitelist")
+
+time.sleep(3)
+
+find_element_by_xpath_v2(driver, '//*[@id="address"]').send_keys(mm_address)
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, '//*[@id="email"]').send_keys(email)
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, '/html/body/div/main/div[2]/main/div/div/div[2]/div/div/main/div/form/p[4]/select/option[2]').click()
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver,'/html/body/div/main/div[2]/main/div/div/div[2]/div/div/main/div/form/p[5]/button').click()
