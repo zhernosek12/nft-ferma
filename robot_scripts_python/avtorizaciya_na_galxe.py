3f96394e58028a4bf7e6bbb5bcb08fa3ec4fc3ba
zhernosek12 <zhernosek12@gmail.com>
2023-01-11 21:49:11 +0300

    Update avtorizaciya_na_galxe.py

--- robot_scripts_python/avtorizaciya_na_galxe.py
+++ robot_scripts_python/avtorizaciya_na_galxe.py
@@ -1,3 +1,6 @@
+# скрипт запускается с http://ferma.zhernosek.xyz и имеет все встроенные модули
+# https://t.me/zhernosek12
+
 metamaskSelenium = MetamaskSelenium(driver, metamask_password)
 
 driver.get("https://galxe.com/")
