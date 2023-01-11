19c5449726066db95655183c75713bb08ec5ecd2
zhernosek12 <zhernosek12@gmail.com>
2023-01-12 00:49:21 +0300

    Create podklyuchenie_tvitera_na_galxe.py

---
+++ robot_scripts_python/podklyuchenie_tvitera_na_galxe.py
@@ -0,0 +1,50 @@
+metamaskSelenium = MetamaskSelenium(driver, metamask_password)
+
+driver.get("https://galxe.com/twitterConnect")
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, '//*[@id="app"]/div[1]/main/div/div/div/div[2]/div/button').click()
+
+time.sleep(2)
+
+driver.switch_to.window(driver.window_handles[1])
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, "//span[contains(text(),'Tweet')]").click()
+
+time.sleep(2)
+
+find_element_by_xpath_v2(driver, '//*[@id="layers"]/div/div[1]/div/div/div/div[2]/a/span').click()
+
+time.sleep(2)
+
+tweet_url = driver.current_url
+
+driver.close()
+
+driver.switch_to.window(driver.window_handles[0])
+
+driver.execute_script("window.scrollTo(0,10)")
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, '//*[@id="app"]/div/main/div/div/div/div[3]/div/div[3]/input').send_keys(tweet_url)
+
+time.sleep(1)
+
+find_element_by_xpath_v2(driver, '//*[@id="app"]/div/main/div/div/div/div[3]/div/button').click()
+
+time.sleep(5)
+
+try:
+    metamaskSelenium.sign_confirm()
+except:
+    pass
+
+time.sleep(1)
+
+driver.get(tweet_url)
+
+print("galaxy --> twitter connected!")
