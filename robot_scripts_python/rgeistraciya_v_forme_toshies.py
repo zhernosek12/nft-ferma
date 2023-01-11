b4ca81272631d6880e9a2e7a7c14d4b8cdac6524
zhernosek12 <zhernosek12@gmail.com>
2023-01-11 22:19:00 +0300

    Update rgeistraciya_v_forme_toshies.py

--- robot_scripts_python/rgeistraciya_v_forme_toshies.py
+++ robot_scripts_python/rgeistraciya_v_forme_toshies.py
@@ -17,9 +17,7 @@ find_element_by_xpath_v2(driver, '/html/body/div/main/div[2]/main/div/div/div[2]
 
 time.sleep(1)
 
-JavascriptExecutor jse = (JavascriptExecutor)driver;
-
-jse.executeScript("scroll(0, 150)");
+driver.execute_script("window.scrollTo(0,150)")
 
 time.sleep(1)
 
