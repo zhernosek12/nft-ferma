0d0fc209bd4a56b7b561de78394647111fbcfbce
zhernosek12 <zhernosek12@gmail.com>
2023-01-12 13:57:32 +0300

    Update avtorizaciya_na_galxe.py

--- robot_scripts_python/avtorizaciya_na_galxe.py
+++ robot_scripts_python/avtorizaciya_na_galxe.py
@@ -9,20 +9,14 @@ driver.minimize_window()
 
 driver.set_window_size(1370, 1010)
 
-try:
-    # после авторизации просит авторизоваться
-    metamaskSelenium.connect_to_website()
-except:
-    pass
+metamaskSelenium.connect_to_website()
 
 time.sleep(1)
 
 driver.get("https://galxe.com/")
 
-try:
-    # кроме этого просит ещё и подключить потом
-    metamaskSelenium.connect_to_website()
-except:
-    pass
+time.sleep(1)
+
+metamaskSelenium.connect_to_website()
 
 time.sleep(1)
